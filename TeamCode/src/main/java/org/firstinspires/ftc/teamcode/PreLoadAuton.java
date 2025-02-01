5a8aad2a48ee7d9cfd0fd9025bae95b9eadf23f2
LaniTamale <leilanitamez@gmail.com>
2025-01-31 22:35:44 -0600

    Changes in driveToPosition Speeds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PreLoadAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PreLoadAuton.java
@@ -24,24 +24,24 @@ public class PreLoadAuton extends LinearOpMode {
 
         // Raise slide and lock miniClaw
         logAndExecute("Raising front slide and toggling claw", () -> {
-            robot.frontSlideToPosition(18, 0.5, true);
+            robot.frontSlideToPosition(18, 0.7, true);
         });
 
         // Drive right
         logAndExecute("Driving right", () -> {
-            robot.driveToPosition(31, 0, 0.5, true);
+            robot.driveToPosition(31, 0, 1.0, true);
         });
 
         // Score the preload object
         logAndExecute("Hanging specimen", () -> {
-            robot.frontSlideToPosition(12, 0.5, true);
+            robot.frontSlideToPosition(12, 0.7, true);
             robot.miniClawServo.setPosition(robot.miniClawOpenPos);
         });
 
         // Park
         logAndExecute("Parking", () -> {
-            robot.driveToPosition(-28, 0, 0.5, true);
-            robot.driveToPosition(0, -44, 0.5, true);
+            robot.driveToPosition(-28, 0, 1.0, true);
+            robot.driveToPosition(0, -44, 1.0, true);
         });
 
         // Completion message
