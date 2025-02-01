4324cfaff5de5c8e1e7d639342d48279d91acc7c
LaniTamale <leilanitamez@gmail.com>
2025-02-01 11:56:41 -0600

    changed drive position slightly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PreLoadAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PreLoadAuton.java
@@ -29,7 +29,7 @@ public class PreLoadAuton extends LinearOpMode {
 
         // Drive right
         logAndExecute("Driving right", () -> {
-            robot.driveToPosition(31, 0, 1.0, true);
+            robot.driveToPosition(35, 0, 1.0, true);
         });
 
         // Score the preload object
