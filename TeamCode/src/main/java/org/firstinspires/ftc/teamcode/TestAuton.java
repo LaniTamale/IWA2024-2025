5584380b9dc481ae58772e44bc0680b12f1813aa
LaniTamale <leilanitamez@gmail.com>
2025-02-07 16:40:15 -0600

    Imported files of Roadrunner, Driving classes, basic hardware configured, updated place holders to measure values for odomitry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
@@ -1,15 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
 @Autonomous(name="TestAuton", group="LinearOpMode")
 // test autonomous code
 public class TestAuton extends LinearOpMode {
     public void runOpMode() {
         Robot robot = new Robot(hardwareMap);
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        Action trajectoryActionForward = drive.actionBuilder(initialPose)
+                .lineToX(24)
+                .build();
 
         waitForStart();
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionForward
+                )
+        );
 
         robot.frontSlideToPosition(8, 0.5, true);
         boolean ret = robot.frontSlideToPosition(8, 0.5, true);
